d842473a8eca731173c141b9c5ac3f9a8f13ebba
Suzzymama <ejirosuzzyakpos@gmail.com>
2022-04-24 14:13:13 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	for (digit = 0; digit <= 9; digit++)
+		printf("%d", digit);
+
+	printf("\n");
+
+	return (0);
+}
