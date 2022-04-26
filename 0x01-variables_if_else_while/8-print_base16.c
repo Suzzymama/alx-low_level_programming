55c7d510ba7e3e6fd16e51e5ef430491a470fa8b
Suzzymama <ejirosuzzyakpos@gmail.com>
2022-04-26 16:09:31 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	char low;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+
+	putchar('\n');
+
+	return (0);
+}
