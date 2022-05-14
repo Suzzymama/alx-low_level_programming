f579f8cd2a49e6aca8a04ff654b9e96b04e59cba
Suzzymama <ejirosuzzyakpos@gmail.com>
2022-05-14 13:16:03 -0700

    fxn_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,37 +1,18 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
-void print_name_as_is(char *name)
+void print_name(char *name, void (*f)(char *))
 {
-    printf("Hello, my name is %s\n", name);
+	if (!name || !f)
+		return;
+	f(name);
 }
 
-/**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
-{
-    unsigned int i;
 
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
+
+
