d2b05bfba693c2de0fa0f2f6674dbd475ed820da
Suzzymama <ejirosuzzyakpos@gmail.com>
2022-06-01 13:42:06 -0700

    variadic_functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: no return.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list pr_all;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char argmt[] = "cifs";
+
+	va_start(pr_all, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (argmt[j])
+		{
+			if (format[i] == t_arg[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(pr_all, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(pr_all, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(pr_all, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(pr_all, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		} i++;
+	}
+	printf("\n"), va_end(pr_all);
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+this task is about creating fuctions with a variable number of arguments
